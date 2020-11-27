32b13a475bcc9f0f3aaa24d693a944c4557d1ce6
AYODEJI <ayodejiv5@gmail.com>
2020-11-27 15:15:34 +0100

    Add mobile navbar

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const tab3 = document.querySelector('.tab3');
 const tabCtrl1 = document.querySelector('.tabCtrl1');
 const tabCtrl2 = document.querySelector('.tabCtrl2');
 const tabCtrl3 = document.querySelector('.tabCtrl3');
-const submit = document.getElementById('submit')
-const email = document.getElementById('email')
+const submit = document.getElementById('submit');
+const email = document.getElementById('email');
+const hamOpen = document.querySelector('.hamburger')
+const closeNav = document.querySelector('.close')
+
+closeNav.addEventListener('click', () => {
+    document.querySelector('.mobile__navbar__container').style.display = 'none';
+    document.querySelector('.mobile__navbar__color').style.fill = '#242A45';
+    document.querySelector('.mobile__navbar__circle-color').style.fill = '#5267DF';
+    document.querySelector('.transparent').style.fill = '#fff';
+    hamOpen.style.display = 'block';
+    closeNav.style.display = 'none';
+    document.querySelector('header').style.background = '#ffffff';
+})
+hamOpen.addEventListener('click', () => {
+    document.querySelector('.mobile__navbar__container').style.display = 'flex';
+    document.querySelector('.mobile__navbar__color').style.fill = '#ffffff';
+    document.querySelector('.mobile__navbar__circle-color').style.fill = '#ffffff';
+    document.querySelector('.transparent').style.fill = 'rgba(47, 53, 79, 1)';
+    closeNav.style.display = 'block';
+    hamOpen.style.display = 'none';
+    document.querySelector('header').style.background = 'rgba(47, 53, 79, 1)';
+})
 
 function tabCtrl(show, hide, hide1) {
     show.style.display = 'block';
@@ -94,3 +115,5 @@ submit.addEventListener("click", (e) => {
 });
 
 email.addEventListener("input", () => { emailValidator(); });
+// document.querySelector('img-1').tabInde;
+// document.querySelector('img-2').tabInde;
